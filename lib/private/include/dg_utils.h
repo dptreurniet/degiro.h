bab6baf75574bcd752a36ac066c0d4b16c8c3ff6
Daan Treurniet <dptreurniet@gmail.com>
2025-10-10 20:11:19 +0200

    - Add dg prefix to util functions

--- lib/private/include/dg_utils.h
+++ lib/private/include/dg_utils.h
@@ -5,12 +5,12 @@
 
 #include "cjson/cJSON.h"
 
-void dump_to_file(const char *str, const char *filename);
-const char *format_string(const char *format, ...);
+void dg__dump_to_file(const char *str, const char *filename);
+const char *dg__format_string(const char *format, ...);
 
-bool parse_string(cJSON *root, const char *json_key, char **destination);
-bool parse_string_to_int(cJSON *root, const char *json_key, int *destination);
-bool parse_int(cJSON *root, const char *json_key, int *destination);
-bool parse_long_long_int(cJSON *root, const char *json_key, long long int *destination);
-bool parse_bool(cJSON *root, const char *json_key, bool *destination);
-bool parse_double(cJSON *root, const char *json_key, double *destination);
+bool dg__parse_string(cJSON *root, const char *json_key, char **destination);
+bool dg__parse_string_to_int(cJSON *root, const char *json_key, int *destination);
+bool dg__parse_int(cJSON *root, const char *json_key, int *destination);
+bool dg__parse_long_long_int(cJSON *root, const char *json_key, long long int *destination);
+bool dg__parse_bool(cJSON *root, const char *json_key, bool *destination);
+bool dg__parse_double(cJSON *root, const char *json_key, double *destination);
